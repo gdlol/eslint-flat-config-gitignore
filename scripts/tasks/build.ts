abb412e6d8861182ed888dc6fba596205c166383
gdlol <gdlol@users.noreply.github.com>
2023-11-02 11:31:29 +0000

    Update build.

--- scripts/tasks/build.ts
+++ scripts/tasks/build.ts
@@ -1,5 +1,5 @@
 import fs from "node:fs";
-import { rm, writeFile } from "node:fs/promises";
+import { copyFile, rm, writeFile } from "node:fs/promises";
 import path from "node:path";
 
 import git from "isomorphic-git";
@@ -13,7 +13,9 @@ import prettierOptions from "../../.config/prettier/.prettierrc.json";
 import pkg from "../../eslint-flat-config-gitignore/package.json";
 import { license } from "../../package.json";
 
-const clean = () => rm(path.resolve(projectRoot, pkg.name, "dist"), { recursive: true, force: true });
+const dist = path.resolve(projectRoot, pkg.name, "dist");
+
+const clean = () => rm(dist, { recursive: true, force: true });
 
 const compile = () => $$`tsc --project ${path.resolve(projectRoot, pkg.name, "tsconfig.json")}`;
 
@@ -22,20 +24,27 @@ const writePackageJson = async () => {
   const url = (await git.listRemotes({ fs, dir: projectRoot })).at(0)?.url;
   const pkgJson: PackageJson = Object.assign({}, pkg as PackageJson, {
     version: semverRsort(tags)[0],
-    repository: url ?? { type: "git", url },
+    repository: url && { type: "git", url },
     license,
     exports: {
       types: "./index.d.ts",
       import: "./index.js",
     },
   });
-  const filepath = path.resolve(projectRoot, pkg.name, "dist/package.json");
+  const filepath = path.resolve(dist, "package.json");
   const packageJson = await prettier.format(JSON.stringify(pkgJson), { ...prettierOptions, filepath });
   await writeFile(filepath, packageJson);
+};
+
+const copyFiles = async (...files: string[]) => {
+  for (const file of files) {
+    await copyFile(path.resolve(projectRoot, file), path.resolve(dist, file));
+  }
 };
 
 export const build = async () => {
   await clean();
   await compile();
   await writePackageJson();
+  await copyFiles("ReadMe.md", "LICENSE");
 };
