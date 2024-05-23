171ec643b2ba3be76da958f5bf07c04c4e040279
gdlol <gdlol@users.noreply.github.com>
2024-05-23 15:48:15 +0000

    update configs.

--- .config/eslint/eslint.config.ts
+++ .config/eslint/eslint.config.ts
@@ -1,47 +1,40 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
 import { FlatCompat } from "@eslint/eslintrc";
 import eslint from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import { gitignore } from "eslint-flat-config-gitignore";
-import globals from "globals";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import vitest from "eslint-plugin-vitest";
 import tsESLint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({ baseDirectory: __dirname });
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
 
 export default tsESLint.config(
-  await gitignore(__dirname),
+  await gitignore(import.meta.dirname),
   {
-    languageOptions: { globals: globals.node },
     linterOptions: { reportUnusedDisableDirectives: true },
   },
   eslint.configs.recommended,
   {
     files: ["**/*.{ts,tsx,cts,mts}"],
     extends: [...tsESLint.configs.recommendedTypeChecked, ...tsESLint.configs.stylisticTypeChecked],
-    languageOptions: {
-      parserOptions: { project: true, tsConfigRootDir: __dirname },
-    },
+    languageOptions: { parserOptions: { project: true, tsConfigRootDir: import.meta.dirname } },
     rules: {
       "@typescript-eslint/consistent-type-exports": "error",
       "@typescript-eslint/consistent-type-imports": "error",
     },
   },
-  ...compat.config({
-    overrides: [
-      {
-        files: ["*.test.{ts,tsx,cts,mts}"],
-        extends: ["plugin:vitest/recommended"],
-      },
-    ],
-  }),
-  ...compat.plugins("import", "simple-import-sort", "unicorn"),
+  {
+    files: ["**/*.test.{ts,tsx,cts,mts}"],
+    extends: [vitest.configs.recommended],
+  },
+  ...compat.plugins("import"),
   {
     files: ["**/*.{ts,tsx,cts,mts}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      unicorn: eslintPluginUnicorn,
+    },
     rules: {
       "import/no-extraneous-dependencies": "error",
       "simple-import-sort/exports": "error",
@@ -51,6 +44,7 @@ export default tsESLint.config(
   },
   {
     files: ["**/*.{ts,tsx,mts}"],
+    plugins: { unicorn: eslintPluginUnicorn },
     rules: { "unicorn/prefer-module": "error" },
   },
   prettier,
