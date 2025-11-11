ece40bb4fd1d265772a87a54565e039425ea499a
gdlol <gdlol@users.noreply.github.com>
2025-11-11 19:26:34 +0000

    fix CI

--- scripts/restore.ts
+++ scripts/restore.ts
@@ -1,8 +1,3 @@
-import { $$ } from "@/scripts/shell.js";
 import { build } from "@/scripts/tasks/build.js";
 
-const codecovVersion = "11.2.5";
-
 await build();
-
-await $$`uv tool install --reinstall codecov-cli@${codecovVersion}`;
