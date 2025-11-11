86803ba62cbd16a231d34552a7f3f77719b09052
gdlol <gdlol@users.noreply.github.com>
2025-11-11 17:06:55 +0000

    testing

--- scripts/codecov.ts
+++ scripts/codecov.ts
@@ -1,16 +1,15 @@
-import fs from "node:fs/promises";
+// import fs from "node:fs/promises";
 import path from "node:path";
 
-import git from "isomorphic-git";
-
+// import git from "isomorphic-git";
 import { projectRoot } from "@/scripts/project.js";
 import { $$ } from "@/scripts/shell.js";
 
-const commitHash = await git.resolveRef({ fs, dir: projectRoot, ref: "HEAD" });
+// const commitHash = await git.resolveRef({ fs, dir: projectRoot, ref: "HEAD" });
 
 const coverageFilePath = path.resolve(projectRoot, "coverage/clover.xml");
 
 await $$`codecovcli --verbose upload-process \
-  --commit-sha ${commitHash} \
+  --auto-load-params-from appveyor \
   --disable-search --fail-on-error \
   --file ${coverageFilePath}`;
