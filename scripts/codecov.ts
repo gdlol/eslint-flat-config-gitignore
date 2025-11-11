9b0374069f123a19c9f8ab47a5dca9fe7085eddb
gdlol <gdlol@users.noreply.github.com>
2025-11-11 17:15:49 +0000

    Fix CI

--- scripts/codecov.ts
+++ scripts/codecov.ts
@@ -1,16 +1,12 @@
-import fs from "node:fs/promises";
 import path from "node:path";
-
-import git from "isomorphic-git";
 
 import { projectRoot } from "@/scripts/project.js";
 import { $$ } from "@/scripts/shell.js";
 
-const commitHash = await git.resolveRef({ fs, dir: projectRoot, ref: "HEAD" });
-
 const coverageFilePath = path.resolve(projectRoot, "coverage/clover.xml");
 
-await $$`codecovcli --verbose upload-process \
-  --commit-sha ${commitHash} \
+const ciProvider = process.env.CI_PROVIDER ?? "local";
+
+await $$`codecovcli --verbose --auto-load-params-from ${ciProvider} upload-process \
   --disable-search --fail-on-error \
   --file ${coverageFilePath}`;
