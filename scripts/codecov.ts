9177ac46a69a022b79c189627423167e52c83600
gdlol <gdlol@users.noreply.github.com>
2025-11-11 18:27:35 +0000

    testing

--- scripts/codecov.ts
+++ scripts/codecov.ts
@@ -10,16 +10,12 @@ const commitHash = await git.resolveRef({ fs, dir: projectRoot, ref: "HEAD" });
 
 const coverageFilePath = path.resolve(projectRoot, "coverage/clover.xml");
 
-const repoProvider = process.env.REPO_PROVIDER ?? "local";
-const projectSlug = process.env.PROJECT_SLUG ?? "local/local";
 const codecovToken = process.env.CODECOV_TOKEN;
 if (!codecovToken) {
   throw new Error("CODECOV_TOKEN is not set");
 }
 
 await $$({ env: { CODECOV_TOKEN: codecovToken } })`codecovcli --verbose upload-process \
-  --git-service ${repoProvider} \
   --commit-sha ${commitHash} \
-  --slug ${projectSlug} \
   --disable-search --fail-on-error \
   --file ${coverageFilePath}`;
