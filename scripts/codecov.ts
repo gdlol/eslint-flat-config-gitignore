f5c3d3c2278164bbb7b6b38f9fad8b7d2a82fda1
gdlol <gdlol@users.noreply.github.com>
2025-11-11 18:24:28 +0000

    testing

--- scripts/codecov.ts
+++ scripts/codecov.ts
@@ -1,12 +1,25 @@
+import fs from "node:fs/promises";
 import path from "node:path";
+
+import git from "isomorphic-git";
 
 import { projectRoot } from "@/scripts/project.js";
 import { $$ } from "@/scripts/shell.js";
 
+const commitHash = await git.resolveRef({ fs, dir: projectRoot, ref: "HEAD" });
+
 const coverageFilePath = path.resolve(projectRoot, "coverage/clover.xml");
 
-const ciProvider = process.env.CI_PROVIDER ?? "local";
+const repoProvider = process.env.REPO_PROVIDER ?? "local";
+const projectSlug = process.env.PROJECT_SLUG ?? "local/local";
+const codecovToken = process.env.CODECOV_TOKEN;
+if (!codecovToken) {
+  throw new Error("CODECOV_TOKEN is not set");
+}
 
-await $$`codecovcli --verbose --auto-load-params-from ${ciProvider} upload-process \
+await $$({ env: { CODECOV_TOKEN: codecovToken } })`codecovcli --verbose upload-process \
+  --git-service ${repoProvider} \
+  --commit-sha ${commitHash} \
+  --slug ${projectSlug} \
   --disable-search --fail-on-error \
   --file ${coverageFilePath}`;
