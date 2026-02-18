0c5af21d05b2d82f837c66441ac94127534f5856
gdlol <gdlol@users.noreply.github.com>
2026-02-18 05:17:26 +0000

    Fix build

--- scripts/tasks/syncpack.ts
+++ scripts/tasks/syncpack.ts
@@ -5,6 +5,6 @@ import { $$ } from "@/scripts/shell.js";
 
 const configPath = path.resolve(projectRoot, ".config/syncpack/config.json");
 
-export const syncpackLint = () => $$`syncpack list-mismatches --config ${configPath}`;
+export const syncpackLint = () => $$`syncpack lint --config ${configPath}`;
 
-export const syncpackFix = () => $$`syncpack fix-mismatches --config ${configPath}`;
+export const syncpackFix = () => $$`syncpack fix --config ${configPath}`;
